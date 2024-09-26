499a305fff8499a13acc5c66d0eb086c9b59abf9
Danielrey99 <danielreyperez99@gmail.com>
2024-09-26 21:35:27 +0200

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,19 +7,38 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
+        List<String> miLista = new ArrayList<>();
+        miLista.add("GKFFD");
+        miLista.add("TNANA");
+        miLista.add("MPMSL");
+        miLista.add("PSWME");
+        miLista.add("LZMLF");
+        miLista.add("JYEBV");
+        miLista.add("YELNT");
+        miLista.add("JSNKR");
+        miLista.add("JFESF");
+        miLista.add("TMJLL");
 
+        return miLista;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (int i = 0; i < customList.size(); i++) {
+            System.out.println("Index: " + i + ", Element: " + customList.get(i));
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
+        List<String> myList = createArrayList();
+        printElementsAndIndex(myList);
 
+        boolean add = addElementToList(myList, "TMJLL");
+        System.out.println("Element added: " + add);
+        printElementsAndIndex(myList);
     }
 
 
